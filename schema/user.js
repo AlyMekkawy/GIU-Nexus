2ebf59a5a28978c78a44bc0891dd39a4c81e6d44
Adham Walaa Elewa <adham.elewa@student.giu-uni.de>
2026-04-16 01:57:33 +0200

    Finished email, password, role, profile picture, createdAt

--- schema/user.js
+++ schema/user.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-  name: {
-      type: String,
-      required: [true, "Name is required"],
-      trim: true,
-      minlength: [2, "Name must be at least 2 characters"],
-      maxlength: [50, "Names cannot exceed 50 characters"],
-  },
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        trim: true,
+        minlength: [2, "Name must be at least 2 characters"],
+        maxlength: [50, "Names cannot exceed 50 characters"],
+    },
     email:{
         required: [true, "Email is required"],
         type: String,
