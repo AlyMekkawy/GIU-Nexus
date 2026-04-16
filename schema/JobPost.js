3e707865b59da9bd99b4e905e08ad12604446774
Sam Bostawros <sam.bostawros@student.giu-uni.de>
2026-04-16 16:05:30 +0200

    Finished my Attributes

--- schema/JobPost.js
+++ schema/JobPost.js
@@ -1,7 +1,29 @@
 const mongoose = require("mongoose");
 
 const jobPostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
 
+  company: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  requirements: {
+    type: [String],
+    required: true,
+    default: []
+  }
 });
 
 module.exports = mongoose.model("JobPost", jobPostSchema);
