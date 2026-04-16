739ba8882b21f7594475bf9957dc62794980506f
Tarek Wael Aboelseoud <tarek.aboelseoud1@gmail.com>
2026-04-16 20:07:44 +0200

    Add location, type, salary, and category fields

--- schema/JobPost.js
+++ schema/JobPost.js
@@ -1,6 +1,28 @@
 const mongoose = require("mongoose");
 
 const jobPostSchema = new mongoose.Schema({
+  location: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  type: {
+    type: String,
+    enum: ["full-time", "part-time", "internship"],
+    required: true
+  },
+
+  salary: {
+    type: Number
+  },
+
+  category: {
+    type: String,
+    enum: ["Frontend", "Backend", "AI/ML", "DevOps", "Data Engineering", "Other"],
+    default: "Other"
+  } 
+
 
 });
 
