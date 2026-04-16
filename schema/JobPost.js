97def6095c094db00e90acdd28dda7face86dc28
David Ramy <david.manoly@giu-uni.de>
2026-04-16 23:34:46 +0200

    finished my attributes

--- schema/JobPost.js
+++ schema/JobPost.js
@@ -1,7 +1,28 @@
 const mongoose = require("mongoose");
 
 const jobPostSchema = new mongoose.Schema({
+totalSlots: {
+    type: Number,
+    required: true,
+    min: 1
+  },
 
+  status: {
+    type: String,
+    enum: ["open", "closed"],
+    default: "open"
+  },
+
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model("JobPost", jobPostSchema);
