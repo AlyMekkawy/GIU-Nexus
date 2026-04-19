dff77bd9813e47c7b92539fded6cc7a63bda556e
Aly Moataz <aly.elmekawy@student.giu-uni.de>
2026-04-19 18:22:35 +0200

    bugfix/ syntax was broken

--- schema/JobPost.js
+++ schema/JobPost.js
@@ -1,66 +1,62 @@
 const mongoose = require("mongoose");
 
 const jobPostSchema = new mongoose.Schema({
-  title: {
-totalSlots: {
-    type: Number,
-    required: true,
-    min: 1
-  },
-  location: {
-    type: String,
-    required: true,
-    trim: true
-  },
-
-  company: {
-    type: String,
-    required: true,
-    trim: true
-  },
-
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-
-  requirements: {
-    type: [String],
-    required: true,
-    default: []
-  type: {
-    type: String,
-    enum: ["full-time", "part-time", "internship"],
-    required: true
-  },
-
-  salary: {
-    type: Number
-  },
-
-  category: {
-    type: String,
-    enum: ["Frontend", "Backend", "AI/ML", "DevOps", "Data Engineering", "Other"],
-    default: "Other"
-  },
-    
-  status: {
-    type: String,
-    enum: ["open", "closed"],
-    default: "open"
-  },
-
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true
-  },
-
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    company: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    requirements: {
+        type: [String],
+        default: [] //required: true not needed if we use default: [] since it will always have a value, even if it's an empty array
+    },
+    location: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    type: {
+        type: String,
+        enum: ["full-time", "part-time", "internship"],
+        required: true
+    },
+    salary: {
+        type: Number
+    },
+    category: {
+        type: String,
+        enum: ["Frontend", "Backend", "AI/ML", "DevOps", "Data Engineering", "Other"],
+        default: "Other"
+    },
+    totalSlots: {
+        type: Number,
+        required: true,
+        min: 1
+    },
+    status: {
+        type: String,
+        enum: ["open", "closed"],
+        default: "open"
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 module.exports = mongoose.model("JobPost", jobPostSchema);
