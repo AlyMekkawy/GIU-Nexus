8e06a9a38b8540837cb8a23b21eb0575c8c1dd73
Youssef-Solimann <youssefasoliman101@gmail.com>
2026-05-01 01:00:09 +0300

    updateProfile & changePassword

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -6,4 +6,11 @@ const router = express.Router();
 // GET /api/v1/profile - private (any authenticated user)
 router.get('/', requireAuthDemo, getProfile);
 
+const { updateProfile } = require('../controllers/profileController');
+// PATCH /api/v1/profile - private (any authenticated user)
+router.patch('/', requireAuthDemo, updateProfile);
+
+const {changePassword} = require('../controllers/profileController');
+// PATCH /api/v1/profile/change-password - private (any authenticated user)
+router.patch('/change-password', requireAuthDemo, changePassword);
 module.exports = router;
