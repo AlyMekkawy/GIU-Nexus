99e6c52d4020c623699a39af3da11097b7972296
Adham Walaa Elewa <adham.elewa@student.giu-uni.de>
2026-05-03 12:51:29 +0300

    Getting user by ID and deleting user

--- controllers/userController.js
+++ controllers/userController.js
@@ -43,4 +43,50 @@ const updateUserStatus = async (req, res) => {
     res.status(200).json({ success: true, user });
 }
 
+const getUserByID = async (req,res,next)=>{
+    try {
+        const user = await User.findById(req.params.id);
+        if(!user){
+            return res.status(404).json({success:false, message:"User not found"})
+        }
+        else {
+            res.status(200).json({
+                success:true,
+                user: {
+                    _id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    bio: user.bio,
+                    skills: user.skills,
+                    profilePicture: user.profilePicture,
+                    role: user.role,
+                    status: user.status,
+                },
+            })
+        }
+    } catch (error) {
+        next(error);
+
+    }
+};
+
+const deleteUser = async (req,res,next) =>{
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if(!user) {
+            return res.status(404).json({success:false, message:"User not found"})
+        }
+        else {
+            res.status(200).json({
+                success:true,
+                message:"User deleted successfully"
+            })
+        }
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = {getUserByID, deleteUser};
 module.exports = { getUsers, updateUserStatus }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,4 +6,14 @@ const { protect, authorize } = require('../middleware/auth');
 router.get('/', protect, authorize('admin'), getUsers);
 router.patch('/:id/status', protect, authorize('admin'), updateUserStatus);
 
+//getting user from id
+const {getUserByID} = require("../controllers/userController");
+const {protect, authorize} = require("../middleware/auth");
+
+//deleting a user
+const {deleteUser} = require("../controllers/userController");
+
+router.get("/:id",protect, authorize("admin"),getUserByID);  //getting user by id
+router.delete("/:id",protect, authorize("admin"),deleteUser);  //deleting user
+
 module.exports = router;
