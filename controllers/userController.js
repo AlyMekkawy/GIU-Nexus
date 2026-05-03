a2e120f4d4caed662d5b59385a26a9315d8642a2
Adham Walaa Elewa <adham.elewa@student.giu-uni.de>
2026-05-03 20:24:57 +0300

    removed extra printed info about user; feature to delete jobpost

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const JobPost = require('../models/JobPost');
 
 const getUserByID = async (req,res,next)=>{
     try {
@@ -12,12 +13,9 @@ const getUserByID = async (req,res,next)=>{
                 user: {
                     _id: user._id,
                     name: user.name,
-                    email: user.email,
-                    bio: user.bio,
-                    skills: user.skills,
-                    profilePicture: user.profilePicture,
                     role: user.role,
                     status: user.status,
+                    email: user.email,
                 },
             })
         }
@@ -29,16 +27,23 @@ const getUserByID = async (req,res,next)=>{
 
 const deleteUser = async (req,res,next) =>{
     try {
-        const user = await User.findByIdAndDelete(req.params.id);
+        // First, find the user to ensure they exist
+        const user = await User.findById(req.params.id);
         if(!user) {
             return res.status(404).json({success:false, message:"User not found"})
         }
-        else {
-            res.status(200).json({
-                success:true,
-                message:"User deleted successfully"
-            })
-        }
+
+        // Delete all job posts created by this user
+        // This ensures data integrity by removing orphaned records (jobpost)
+        await JobPost.deleteMany({ createdBy: req.params.id });
+
+        // Now delete the user
+        await User.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({
+            success:true,
+            message:"User deleted"
+        })
 
     } catch (error) {
         next(error);
