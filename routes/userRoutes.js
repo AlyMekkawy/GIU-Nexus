fb810c3b7e57ab88276c2c51bd7da16f65ba2ebb
Adham Walaa Elewa <adham.elewa@student.giu-uni.de>
2026-05-03 13:27:39 +0300

    Getting user by ID and deleting user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,5 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 // TODO: Add user routes here
+//getting user from id
+const {getUserByID} = require("../controllers/userController");
+const {protect, authorize} = require("../middleware/auth");
+
+//deleting a user
+const {deleteUser} = require("../controllers/userController");
+
+router.get("/:id",protect, authorize("admin"),getUserByID);  //getting user by id
+router.delete("/:id",protect, authorize("admin"),deleteUser);  //deleting user
 
 module.exports = router;
--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(express.json());
 app.use('/api/v1/auth',    authRoutes);
 app.use('/api/v1/jobs',    jobRoutes);
 app.use('/api/v1/profile', profileRoutes);
+app.use('/api/v1/users', userRoutes);
 
 // Health-check
 app.get('/', (req, res) => {
