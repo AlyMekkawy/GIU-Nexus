895bb863b8a169d35abc9e4eb222123205c061b6
Aly Moataz <aly.elmekawy@student.giu-uni.de>
2026-05-05 09:31:06 +0300

    Revert "Feature 1 DONE"
    This reverts commit 2766467ef3cf60f069b326afd837b7f6abae284d.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { getAdminStats } = require('../controllers/userController');
-
-router.get('/admin/stats', protect, authorize('admin'), getAdminStats);
 
 //getting user from id
 const {getUserByID, deleteUser} = require("../controllers/userController");
