e835e0470b6d179745b7e2b97390f49514ed45bb
Aly Moataz <aly.elmekawy@student.giu-uni.de>
2026-05-05 09:31:07 +0300

    Revert "Feature 1 Redone"
    This reverts commit 65d6122752c0869c1d3e7d60c944c0c747ce30fd.

--- controllers/userController.js
+++ controllers/userController.js
@@ -99,45 +99,4 @@ const updateUserStatus = async (req, res, next) => {
     }
 }
 
-const getAdminStats = async (req, res, next) => {
-    try {
-        const fourWeeksAgo = new Date();
-        fourWeeksAgo.setDate(fourWeeksAgo.getDate() - 28);
-
-        const jobsPerWeek = await JobPost.aggregate([
-            {
-                $match: {
-                    createdAt: { $gte: fourWeeksAgo }
-                }
-            },
-            {
-                $group: {
-                    _id: {
-                        $dateToString: {
-                            format: "%Y-%U",
-                            date: "$createdAt"
-                        }
-                    },
-                    count: { $sum: 1 }
-                }
-            },
-            { $sort: { _id: 1 } }
-        ]);
-
-        res.status(200).json({
-            success: true,
-            jobsPerWeek
-        });
-
-    } catch (error) {
-        next(error);
-    }
-};
-
-module.exports = { 
-    getUsers, 
-    updateUserStatus, 
-    getUserByID, 
-    deleteUser,
-    getAdminStats
-}
+module.exports = { getUsers, updateUserStatus, getUserByID, deleteUser }
