9ccce682bb5f22cb1983de764cec257f8a57f3c8
khaled <69255060+KhaledAhmedElmasry@users.noreply.github.com>
2026-05-15 22:36:42 +0300

    login page and forgot password page done
    login page and forgot password page done also edited something in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
 
-  // ai generated to fix login
-  server: {
-    proxy: {
-      
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
+export default ({ mode }) => {
+ 
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    plugins: [react()],
+
+    //idk if this is needed
+    
+    define: {
+      'import.meta.env.BACKEND_URL': JSON.stringify(env.BACKEND_URL || ''),
+    },
+
+   
+    server: {
+      proxy: {
+        '/api': {
+       
+          target: env.BACKEND_URL || 'http://localhost:5000',
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-})
+  })
+}
