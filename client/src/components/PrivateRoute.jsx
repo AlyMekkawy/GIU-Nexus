508dcd9085a9ad89df7d59858cac335a8c440466
SamirSherif16001872 <samir.sherif@student.giu-uni.de>
2026-05-17 19:14:59 +0300

    corrected

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,22 +1,9 @@
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 function PrivateRoute({ children }) {
     const { isAuthenticated } = useAuth();
-    const location = useLocation();
-
-    if (!isAuthenticated) {
-        const searchParams = new URLSearchParams(location.search);
-        const devPreview = searchParams.get("devPreview");
-        const previewUsed = sessionStorage.getItem("devPreviewUsedChangePassword");
-
-        if (devPreview === "1" && previewUsed !== "true" && location.pathname === "/profile/change-password") {
-            sessionStorage.setItem("devPreviewUsedChangePassword", "true");
-            return children;
-        }
-
-        return <Navigate to="/login" replace />;
-    }
+    if (!isAuthenticated) return <Navigate to="/login" replace />;
     return children;
 }
 
